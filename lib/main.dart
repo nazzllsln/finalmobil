f4c5574988e931f812986e2a671b974073a7033a
nazzllsln <nazl.sln16@gmail.com>
2024-06-09 11:41:38 +0300

    önemli kütüphanelerimi ekledim

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'naazlı aslan demir',
+      title: 'nazlı aslan demir',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'naazlı aslan demir'),
+      home: const MyHomePage(title: 'nazlı aslan demir'),
     );
   }
 }
