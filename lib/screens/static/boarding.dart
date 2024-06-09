3aa5e81900b56b6f161ca70f6ee936972d45c729
nazzllsln <nazl.sln16@gmail.com>
2024-06-09 18:08:35 +0300

    boarding hatası çözüldü.

--- lib/screens/static/boarding.dart
+++ lib/screens/static/boarding.dart
@@ -11,25 +11,28 @@ class _BoardingScreenState extends State<BoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: PageView(
-        children: [
-          SizedBox.expand(
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/images/boarding1.jpg",
-                  width: 50,
-                ),
-                const Text("Buy All You Need!!!")
-              ],
+      body: SafeArea(
+        child: PageView(
+          children: [
+            SizedBox.expand(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    "https://www.macworld.com/wp-content/uploads/2023/01/imac_21_5_2019_with_mba2019_04-1.jpg?quality=50&strip=all",
+                    height: 200, // Yükseklik ekleyelim
+                  ),
+                  const SizedBox(height: 20),
+                  const Text("Buy All You Need!!!")
+                ],
+              ),
             ),
-          ),
-          Center(child: Text("Boarding 1")),
-          Center(child: Text("Boarding 2")),
-          Center(child: Text("Boarding 3")),
-        ],
+            Center(child: Text("Boarding 1")),
+            Center(child: Text("Boarding 2")),
+            Center(child: Text("Boarding 3")),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
